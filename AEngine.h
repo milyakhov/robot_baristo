2283854d810fffdbd9fc8313384d44c7d48b414c
milyakhov <mironlyakhov2005@mail.ru>
2025-05-02 15:48:27 +0300

    Delete AEngine.h

--- AEngine.h
+++
@@ -1,8 +0,0 @@
-class Engine {
-public:
-    virtual ~Engine() = default;
-    virtual void forward(int time_ms) = 0;
-    virtual void left(int time_ms) = 0;
-    virtual void right(int time_ms) = 0;
-    virtual void stop() = 0;
-};
