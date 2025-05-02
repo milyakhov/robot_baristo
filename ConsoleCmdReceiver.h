58d32b0f7b69283cd6a42296df904d9c7ab80002
milyakhov <mironlyakhov2005@mail.ru>
2025-05-02 15:48:39 +0300

    Delete ConsoleCmdReceiver.h

--- ConsoleCmdReceiver.h
+++
@@ -1,9 +0,0 @@
-#import ACmdReceiver.h
-class ConsoleCmdReceiver : public ACmdReceiver {
-public:
-    std::string receive() override {
-        std::string cmd;
-        std::getline(std::cin, cmd);
-        return cmd;
-    }
-};
